bdf47b0af1ef357effdf9c0da1f4c17a0054e4b8
Vitiv <30374212+Vitiv@users.noreply.github.com>
2024-08-20 18:23:12 +0300

    Delete deps_deploy.sh

--- deps_deploy.sh
+++
@@ -1,8 +0,0 @@
-# Start the local replica
-dfx start --background
-
-# Locally deploy the `evm_rpc` canister
-dfx deps pull
-dfx deps init evm_rpc --argument '(record { nodesInSubnet = 28 })'
-dfx deps deploy
-
